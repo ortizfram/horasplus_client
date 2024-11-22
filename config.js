cc4897d0ddc1889ee0ded9ba7727de2a55769cc5
Franco Ortiz <ortizfranco48@yahoo.com>
2024-11-22 13:33:42 -0300

    config ENV

--- config.js
+++ config.js
@@ -1,16 +1,6 @@
-// import dotenv from 'dotenv';
 
-// // Import dotenv only if running in Node.js
-// if (typeof process !== "undefined" && process.env.NODE_ENV !== "production") {
-//  const dotenv = require("dotenv");
-//  dotenv.config();
-// }
 
-// // Fallbacks for environment variables
-// export const RESP_URL = "https://horasplus-api-prod.onrender.com"// process.env.RESP_URL || "http://localhost:8000";
-// export const REQ_URL = process.env.REQ_URL || "http://localhost:8081";
+import { RESP_URL_ENV, REQ_URL_ENV } from '@env';//react native dotenv y babel config
 
-import { RESP_URL, REQ_URL } from '@env';
-
-export const RESP_URL = RESP_URL || "http://localhost:8000";
-export const REQ_URL = REQ_URL || "http://localhost:8081";
+export const RESP_URL = RESP_URL_ENV || "http://localhost:8000";
+export const REQ_URL = REQ_URL_ENV || "http://localhost:8081";
