ec4d0f62649cbe9de6959ee9994a1d323d143a44
Franco Ortiz <ortizfranco48@yahoo.com>
2024-11-25 18:52:10 -0300

    orgid layout

--- app/[orgId]/_layout.jsx
+++ app/[orgId]/_layout.jsx
@@ -1,31 +1,19 @@
-// import { Tabs } from "expo-router";
-// import React, { useContext } from "react";
-// import { MaterialIcons } from "@expo/vector-icons";
-// import { AuthContext } from "../../context/AuthContext";
-// const userInfo = useContext(AuthContext);
+import { Stack } from "expo-router";
 
-// export default function OrgDetailLayout() {
-
-//   return (
-//     <Tabs>
-//       <Tabs.Screen
-//         name="dashboard"
-//         options={{
-//           headerShown: false,
-//           tabBarIcon: ({ color, focused }) => (
-//             <MaterialIcons name={"dashboard"} size={24} color={color} />
-//           ),
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="settings"
-//         options={{
-//           headerShown: false,
-//           tabBarIcon: ({ color, focused }) => (
-//             <MaterialIcons name={"settings"} size={24} color={color} />
-//           ),
-//         }}
-//       />
-//     </Tabs>
-//   );
-// }
+export default function HomeLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="dashboard" />
+    </Stack>
+  );
+}
