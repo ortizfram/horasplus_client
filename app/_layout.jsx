bbaf650bbc3d14f581965f3608e5f5de66833792
Franco Ortiz <ortizfranco48@yahoo.com>
2024-11-25 13:00:17 -0300

    layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,67 +1,116 @@
-import { Slot, Stack, useRouter } from "expo-router";
+import { router, Slot, Stack, useRouter } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useContext } from "react";
-import { AuthProvider, AuthContext } from "../context/AuthContext";
+import "react-native-reanimated";
+
+import { AuthContext, AuthProvider } from "../context/AuthContext";
+import { ActivityIndicator, Text } from "react-native-web";
+import { Pressable, StyleSheet, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import BackButtonLayout from "../components/GoBackButton";
 import Loader from "../components/Loader";
 
-// Prevent splash screen from auto-hiding
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
+  const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   useEffect(() => {
-    if (fontsLoaded) {
+    if (loaded) {
       SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
+  }, [loaded]);
 
-  if (!fontsLoaded) {
-    return <Loader />;
+  if (!loaded) {
+    < Loader />
   }
 
   return (
-    <AuthProvider>
-      <AppNavigator />
+    <AuthProvider style={styles.container}>
+      <Layout />
+      <BackButtonLayout />
     </AuthProvider>
   );
 }
 
-function AppNavigator() {
+function Layout() {
   const { userInfo, splashLoading } = useContext(AuthContext);
-  const router = useRouter();
 
   useEffect(() => {
-    if (!splashLoading && !userInfo?.token) {
-      router.replace("/auth/login");
+    if (userInfo == null) {
+      // router.push("/auth/login");
     }
   }, [splashLoading, userInfo]);
 
-  if (splashLoading) {
-    return <Loader />;
-  }
-
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {userInfo?.token ? (
-        <>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="organization" />
-          <Stack.Screen name="organization/create" />
-        </>
-      ) : (
-        <>
-          <Stack.Screen name="auth/reset" />
-          <Stack.Screen name="auth/sent" />
-          <Stack.Screen name="auth/changed" />
-          <Stack.Screen name="auth/forgot" />
-          <Stack.Screen name="auth/login" />
-          <Stack.Screen name="auth/signup" />
-        </>
-      )}
-    </Stack>
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        {splashLoading ? (
+          <Stack.Screen
+            name="splashScreen"
+            screenOptions={{ headerShown: false, title: "" }}
+          />
+        ) : userInfo?.token ? (
+          <>
+            <Stack.Screen
+              name="(tabs)"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+            <Stack.Screen
+              name="organization"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="auth/signup"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+            <Stack.Screen
+              name="auth/login"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+            <Stack.Screen
+              name="auth/forgot"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+            <Stack.Screen
+              name="auth/sent"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+            <Stack.Screen
+              name="auth/reset"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+            <Stack.Screen
+              name="auth/changed"
+              screenOptions={{ headerShown: false, title: "" }}
+            />
+          </>
+        )}
+
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      {/* <Slot />{" "} */}
+      {/* This Slot will render the dynamic route based on the navigation */}
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+});
