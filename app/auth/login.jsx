004b974f80e05f2568c784f149ea15d93cd65319
Franco Ortiz <ortizfranco48@yahoo.com>
2024-11-29 18:49:52 -0300

    login Logo debugger

--- app/auth/login.jsx
+++ app/auth/login.jsx
@@ -34,17 +34,17 @@ const Login = () => {
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
-      <Logo
-  style={{
-    width: "100%", // Makes the image take full width of the container
-    maxWidth: 600, // Set a max width to prevent it from becoming too large
-    height: 240,
-    alignSelf: "center",
-    marginBottom: 15,
-    borderWidth: 1, // Debugging border
-    borderColor: "red", // Debugging border
-  }}
-/>
+        <Logo
+          style={{
+            width: "100%", // Makes the image take full width of the container
+            maxWidth: 600, // Set a max width to prevent it from becoming too large
+            height: 240,
+            alignSelf: "center",
+            marginBottom: 15,
+            borderWidth: 1, // Debugging border
+            borderColor: "red", // Debugging border
+          }}
+        />
         <Text style={styles.header}>Ingreso</Text>
         {error ? <Text style={styles.error}>{error}</Text> : null}
       </View>
