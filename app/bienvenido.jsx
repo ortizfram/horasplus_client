1fc0de1c1388123efe96d80afcbb5b6f53b72b03
Franco Ortiz <ortizfranco48@yahoo.com>
2025-01-02 16:50:35 -0300

    image contain

--- app/bienvenido.jsx
+++ app/bienvenido.jsx
@@ -359,7 +359,7 @@ const styles = StyleSheet.create({
   carouselImage: {
     width: "100%",
     height: "100%",
-    resizeMode: "cover",
+    resizeMode: "contain", 
   },
   carouselTextContainer: {
     position: "absolute",
