d2137b6789c1866e63a440f1b5c09d7023956237
Franco Ortiz <ortizfranco48@yahoo.com>
2025-01-07 10:59:39 -0300

    GOOGLE_MAPS_API_KEY_ENV

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 
-import { RESP_URL_ENV, REQ_URL_ENV,GOOGLE_MAPS_API_KEY } from '@env';//react native dotenv y babel config
+import { RESP_URL_ENV, REQ_URL_ENV,GOOGLE_MAPS_API_KEY_ENV } from '@env';//react native dotenv y babel config
 
 export const RESP_URL = RESP_URL_ENV || "http://localhost:8000";
 export const REQ_URL = REQ_URL_ENV || "http://localhost:8081";
-export const GOOGLE_MAPS_API_KEY = GOOGLE_MAPS_API_KEY || "GOOGLE_MAPS_API_KEY";
+export const GOOGLE_MAPS_API_KEY = GOOGLE_MAPS_API_KEY_ENV || "GOOGLE_MAPS_API_KEY";
