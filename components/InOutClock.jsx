cb08c459fc6ea7aef81ebf36a5343af042244e53
Franco Ortiz <ortizfranco48@yahoo.com>
2025-01-11 09:09:20 -0300

    removed twilio

--- components/InOutClock.jsx
+++ components/InOutClock.jsx
@@ -19,7 +19,7 @@ const InOutClock = ({ orgId, setShowSearch }) => {
   const [wasIn, setWasIn] = useState(false);
   const [loading, setLoading] = useState(true);
   const [screenMessage, setScreenMessage] = useState(null);
-  const client = new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+  // const client = new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 
   const fetchOrg = async () => {
@@ -121,14 +121,15 @@ const InOutClock = ({ orgId, setShowSearch }) => {
 
     const orgAdminCellphones = org.admin_celphones.map((number) => number);
     orgAdminCellphones.forEach((phone) => {
-      client.messages
-        .create({
-          body: message,
-          to: phone, // Text this number
-          from: TWILIO_PHONE, // From a valid Twilio number
-        })
-        .then((message) => console.log(message.sid))
-        .catch((error) => console.error(error));
+      // client.messages
+      //   .create({
+      //     body: message,
+      //     to: phone, // Text this number
+      //     from: TWILIO_PHONE, // From a valid Twilio number
+      //   })
+      //   .then((message) => console.log(message.sid))
+      //   .catch((error) => console.error(error));
+      console.log("Message sent to:", phone);
     });
   };
 
